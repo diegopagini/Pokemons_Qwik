11cb77019da690557e78ad31d56e5be7fb90062f
Diego Pagini <diegopaginidev@gmail.com>
2023-05-15 07:12:17 +0200

    lazy load

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,17 @@
 /** @format */
-import { component$, useSignal } from '@builder.io/qwik';
+import { $, component$, useSignal } from '@builder.io/qwik';
 
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
 	const pokemonId = useSignal<number>(1); // For primitives: booleans, strings, numbers, etc... For objects or arrays `useStore()`
+
+	const changePokemonId = $((value: number) => {
+		// Because this function is going to be called in a lazy load way it needs to be serialized with `$()`
+		if (pokemonId.value + value <= 0) return;
+
+		pokemonId.value += value;
+	});
 
 	return (
 		<>
@@ -12,9 +19,27 @@ export default component$(() => {
 			<span class='text-9xl'>{pokemonId}</span>
 			{/** Qwik knows when is a singal and there is no need to `call it` like pokemonId() */}
 
+			<img
+				alt='Pokemon Sprite'
+				height='200px'
+				src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId.value}.png`} /** When the value
+				of the signal is needed to use it in a string, we must use the value */
+				width='200px'
+			/>
+
 			<div class='mr-2'>
-				<button class='btn btn-primary mr-2'>Anterior</button>
-				<button class='btn btn-primary'>Siguientes</button>
+				<button
+					onClick$={() => changePokemonId(-1)}
+					class='btn btn-primary mr-2'
+				>
+					Anterior
+				</button>
+				<button
+					onClick$={() => changePokemonId(+1)}
+					class='btn btn-primary'
+				>
+					Siguiente
+				</button>
 			</div>
 		</>
 	);
