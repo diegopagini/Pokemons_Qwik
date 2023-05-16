c39cc442bf9e8dc6b591b8d55a0a3470dbf55f21
Diego Pagini <diego.pagini@neoris.com>
2023-05-16 07:13:38 +0200

    routeLoader

--- README.md
+++ README.md
@@ -245,3 +245,31 @@ export default component$(() => {
 	);
 });
 ```
+
+### routeLoader
+
+```tsx
+/**
+ * routeLoader$ is going to be executed from the server side. Is used to prevent navigation with incorrect params.
+ */
+export const usePokemonId = routeLoader$<number>(({ params, redirect }) => {
+	const id = Number(params.id);
+
+	if (isNaN(id)) redirect(301, '/');
+
+	if (id <= 0 || id > 1000) redirect(301, '/');
+
+	return id;
+});
+
+export default component$(() => {
+	const pokemonId = usePokemonId();
+
+	return (
+		<>
+			<span class='text-5xl'>Pokemon: {pokemonId}</span>
+			<PokemonImage id={pokemonId.value} />
+		</>
+	);
+});
+```
--- src/routes/pokemon/[id]/index.tsx
+++ src/routes/pokemon/[id]/index.tsx
@@ -1,16 +1,28 @@
 /** @format */
-import { component$ } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
+import { component$, Signal } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
 import { PokemonImage } from '~/components/pokemons/pokemon-image';
 
+/**
+ * routeLoader$ is going to be executed from the server side. Is used to prevent navigation with incorrect params.
+ */
+export const usePokemonId = routeLoader$<number>(({ params, redirect }) => {
+	const id = Number(params.id);
+
+	if (isNaN(id)) redirect(301, '/');
+
+	if (id <= 0 || id > 1000) redirect(301, '/');
+
+	return id;
+});
+
 export default component$(() => {
-	const location = useLocation();
-	const { id } = location.params;
+	const pokemonId: Signal<number> = usePokemonId();
 
 	return (
 		<>
-			<span class='text-5xl'>Pokemon: {id}</span>
-			<PokemonImage id={+id} />
+			<span class='text-5xl'>Pokemon: {pokemonId}</span>
+			<PokemonImage id={pokemonId.value} />
 		</>
 	);
 });
