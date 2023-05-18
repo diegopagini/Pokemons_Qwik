3645ee693805cd074d56e4c0f19b958543ddd5b0
Diego Pagini <diego.pagini@neoris.com>
2023-05-18 07:46:42 +0200

    provider

--- src/context/pokemon/pokemon.provider.tsx
+++ src/context/pokemon/pokemon.provider.tsx
@@ -1,5 +1,5 @@
 /** @format */
-import { component$, Slot, useContextProvider, useStore } from '@builder.io/qwik';
+import { component$, Slot, useContextProvider, useStore, useVisibleTask$ } from '@builder.io/qwik';
 
 import { PokemonGameContext, PokemonListContext } from '..';
 
@@ -21,10 +21,34 @@ export const PokemonProvider = component$(() => {
 	useContextProvider(PokemonGameContext, pokemonGame);
 	useContextProvider(PokemonListContext, pokemonList);
 
-	return <Slot />; /** Slot is like children
-   to use it like this:
-   <PokemonProvider>
-      <div>...</div>
-   </PokemonProvider>
-  */
+	/** useVisibleTask To run tasks only on the client side */
+	useVisibleTask$(() => {
+		// Without any "track" this is going to be called only one time.
+		if (localStorage.getItem('pokemon-game')) {
+			const {
+				isPokemonVisible = true,
+				pokemonId = 6,
+				showBackImage = false,
+			} = JSON.parse(localStorage.getItem('pokemon-game')!) as PokemonGameState;
+
+			pokemonGame.isPokemonVisible = isPokemonVisible;
+			pokemonGame.pokemonId = pokemonId;
+			pokemonGame.showBackImage = showBackImage;
+		}
+	});
+
+	useVisibleTask$(({ track }) => {
+		// To track every property in the pokemonGame storage.
+		track(() => [pokemonGame.isPokemonVisible, pokemonGame.pokemonId, pokemonGame.showBackImage]);
+
+		localStorage.setItem('pokemon-game', JSON.stringify(pokemonGame));
+	});
+
+	return <Slot />;
+	/** Slot is like children
+	 * to use it like this:
+	 * <PokemonProvider>
+	 *    <div>...</div>
+	 * </PokemonProvider>
+	 */
 });
