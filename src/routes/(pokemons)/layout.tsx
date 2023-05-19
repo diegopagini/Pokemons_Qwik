39c5d618afe4a85c0c74ac25d3fb3251cdaf1e1d
Diego Pagini <diego.pagini@neoris.com>
2023-05-19 08:42:20 +0200

    routes

---
+++ src/routes/(pokemons)/layout.tsx
@@ -0,0 +1,15 @@
+/** @format */
+import { component$, Slot } from '@builder.io/qwik';
+import Navbar from '~/components/shared/navbar/navbar';
+import { PokemonProvider } from '~/context';
+
+export default component$(() => {
+	return (
+		<PokemonProvider>
+			<Navbar />
+			<main class='flex flex-col items-center justify-center'>
+				<Slot />
+			</main>
+		</PokemonProvider>
+	);
+});
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,19 +1,10 @@
 /** @format */
 import { component$, Slot, useStyles$ } from '@builder.io/qwik';
-import Navbar from '~/components/shared/navbar/navbar';
-import { PokemonProvider } from '~/context';
 
 import styles from './styles.css?inline';
 
 export default component$(() => {
 	useStyles$(styles);
 
-	return (
-		<PokemonProvider>
-			<Navbar />
-			<main class='flex flex-col items-center justify-center'>
-				<Slot />
-			</main>
-		</PokemonProvider>
-	);
+	return <Slot />;
 });
