347788e7c8384b3d8e112df6c10d7e1b1f2faefc
Diego Pagini <diegopaginidev@gmail.com>
2023-05-21 17:26:11 +0200

    validations

--- src/routes/(auth)/login/index.tsx
+++ src/routes/(auth)/login/index.tsx
@@ -1,26 +1,34 @@
 /** @format */
 import { component$, useStylesScoped$ } from '@builder.io/qwik';
-import { Form, routeAction$ } from '@builder.io/qwik-city';
+import { Form, routeAction$, z, zod$ } from '@builder.io/qwik-city';
 
 import styles from './login.css?inline';
 
-export const useLoginUserAction = routeAction$((data, { cookie, redirect }) => {
-	const { email, password } = data;
+export const useLoginUserAction = routeAction$(
+	(data, { cookie, redirect }) => {
+		const { email, password } = data;
 
-	if (email === 'test@test.com' && password === '123456') {
-		cookie.set('jwt', 'secret_token', { secure: true, path: '/' });
-		redirect(302, '/');
+		if (email === 'test@test.com' && password === '123456') {
+			cookie.set('jwt', 'secret_token', { secure: true, path: '/' });
+			redirect(302, '/');
+
+			return {
+				success: true,
+				jwt: 'secret_token',
+			};
+		}
 
 		return {
-			success: true,
-			jwt: 'jwt_secret',
+			success: false,
 		};
-	}
+	},
 
-	return {
-		success: false,
-	};
-});
+	// Validations:
+	zod$({
+		email: z.string().email('Formato no valido'),
+		password: z.string().min(6, 'Mínimo 6 caracteres'),
+	})
+);
 
 export default component$(() => {
 	useStylesScoped$(styles);
